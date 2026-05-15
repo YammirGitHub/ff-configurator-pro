ed1c87590b9950bea8e9394316600bcc9e240a46
Yammir <yammir@MacBook-Pro-de-Yammir.local>
2026-05-14 23:04:15 -0500

    1111e2222

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,13 +56,7 @@ export const metadata: Metadata = {
   },
 };
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  viewportFit: "cover",
-};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
